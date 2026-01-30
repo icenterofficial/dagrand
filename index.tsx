f3caf6d01f5a7de1ae0a4fc52afb3d91c542d9a3
iCenter <92814900+icenterofficial@users.noreply.github.com>
2026-01-30 15:59:04 +0700

    Update index.tsx
    Signed-off-by: iCenter <92814900+icenterofficial@users.noreply.github.com>

--- index.tsx
+++ index.tsx
@@ -1,16 +1,15 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { PWAProvider } from './context/PWAContext';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { PWAProvider } from './context/PWAContext.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <PWAProvider>
