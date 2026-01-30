22af11474e8e5fc00a2d7965a252f9aa5a233435
iCenter <92814900+icenterofficial@users.noreply.github.com>
2026-01-30 11:03:08 +0700

    Update index.tsx
    Signed-off-by: iCenter <92814900+icenterofficial@users.noreply.github.com>

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { PWAProvider } from './context/PWAContext';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
